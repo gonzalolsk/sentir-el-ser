c3a3abe75d0eee96f15e816abfb8c49aa314097c
gonzalolsk <gonzalolsk@gmail.com>
2021-10-22 11:38:53 -0300

    home cards

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,6 @@
 const controller = {
-    home: (req, res) => { res.render('home')}
+    home: (req, res) => { res.render('home')},
+    aboutUs: (req,res) => { res.render('about-us')},
+    formaciones: (req, res) => { res.render('formaciones')}
 }
 module.exports = controller
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const mainController = require("../controllers/mainController")
 
 router.get('/', mainController.home);
+router.get('/sobre-nosotros', mainController.aboutUs);
+router.get('/formaciones', mainController.formaciones)
 
 module.exports = router;
