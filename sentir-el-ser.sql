f3195318673f738e21c52acdb0622b6f41744af8
gonzalolsk <gonzalolsk@gmail.com>
2022-05-12 13:36:06 -0300

    sql fix

--- sentir-el-ser.sql
+++ sentir-el-ser.sql
@@ -8,7 +8,7 @@ CREATE TABLE `product_type`(
     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `name` varchar(50) NOT NULL,
     `long_description` text DEFAULT NULL,
-    `short_description` text DEFAULT NULL,
+    `short_description` text DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `teachers`;
@@ -18,8 +18,7 @@ CREATE TABLE `teachers`(
     `last_name` varchar(20) NOT NULL,
     `long_description` text DEFAULT NULL,
     `short_description` text DEFAULT NULL,
-    `img` varchar(255) DEFAULT NULL,
-
+    `img` varchar(255) DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS `products`;
@@ -36,16 +35,15 @@ create table `products` (
   `created_at` TIMESTAMP default CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `baja` TINYINT(1) DEFAULT 0,
-  
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`product_type`) REFERENCES `product_type` (`id`),
-  FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`),
+  FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`id`),
+  FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`)
 );
 
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` INT AUTO_INCREMENT NOT NULL,
+  `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `email` varchar(24) NOT NULL,
@@ -57,19 +55,17 @@ CREATE TABLE `users` (
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `condiciones` TINYINT (1) NOT NULL,
   `baja` TINYINT (1) DEFAULT 0,
-  `products_purchaced` TINYINT (1) DEFAULT NULL, 
+  `products_purchaced` INT UNSIGNED DEFAULT NULL, 
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`products_purchaced`) REFERENCES `products` (`id`),
+  FOREIGN KEY (`products_purchaced`) REFERENCES `products` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-
-
 
 
 
 DROP TABLE IF EXISTS `shopping_cart`;
 CREATE TABLE `shopping_cart` (
 	  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  	`user_id` 		INT	NOT NULL,
+  	`user_id` 		INT	UNSIGNED NOT NULL,
   	`products_id`	INT UNSIGNED NOT NULL,
   	`created_at`	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   	`updated_at`	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -77,20 +73,20 @@ CREATE TABLE `shopping_cart` (
   	`actual_price`	FLOAT(8,2) NOT NULL,
   	`active`		TINYINT (1) NOT NULL,
   	PRIMARY KEY (`id`),
-  	FOREIGN KEY (`users_id`) REFERENCES	`users` (`id`),
+  	FOREIGN KEY (`user_id`) REFERENCES	`users` (`id`),
   	FOREIGN KEY (`products_id`) REFERENCES `products`(`id`)
 );
 
 DROP TABLE IF EXISTS `post_category`;
 CREATE TABLE `post_category`(
-    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
     `name` varchar(50) NOT NULL,
-  	PRIMARY KEY (`id`),
+  	PRIMARY KEY (`id`)
 );
 
 DROP TABLE IF EXISTS `blog_posts`;
 CREATE TABLE `blog_posts`(
-    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
     `title` varchar(20) NOT NULL,
     `brief` text DEFAULT NULL,
     `content` text DEFAULT NULL,
@@ -100,25 +96,23 @@ CREATE TABLE `blog_posts`(
     `comments_id` text DEFAULT NULL,
     `created_at` TIMESTAMP default CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
   	PRIMARY KEY (`id`),
   	FOREIGN KEY (`post_category_id`) REFERENCES	`post_category` (`id`),
-  	FOREIGN KEY (`teacher_id`) REFERENCES `teacher`(`id`)
+  	FOREIGN KEY (`teacher_id`) REFERENCES `teachers`(`id`)
 );
 
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments`(
-    `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
     `comment` varchar(20) NOT NULL,
     `user_id` INT UNSIGNED not null,
     `post_id` INT UNSIGNED not null,
     `created_at` TIMESTAMP default CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
   	PRIMARY KEY (`id`),
-  	FOREIGN KEY (`user_id`) REFERENCES	`user` (`id`),
-  	FOREIGN KEY (`post_id`) REFERENCES `post`(`id`)
+  	FOREIGN KEY (`user_id`) REFERENCES	`users` (`id`),
+  	FOREIGN KEY (`post_id`) REFERENCES `blog_posts`(`id`)
 );
 
 
